c12dbb119ddf65e535efbe0432fe2860d0428a83
alexzhuravlov13 <alexzhuravlov13@gmail.com>
2020-07-23 09:41:41 +0300

    chooser fragment

--- app/src/main/java/com/keepsolid/gittestapp/activity/FirstActivity.java
+++ app/src/main/java/com/keepsolid/gittestapp/activity/FirstActivity.java
@@ -21,9 +21,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FirstActivity extends BaseActivity {
-    private RecyclerView recyclerView;
-    private List<Smartphone> smartphones;
-    private SmartphoneRecyclerAdapter adapter;
+    private ChooserFragment chooserFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,23 +30,7 @@ public class FirstActivity extends BaseActivity {
 
         initToolbar(getString(R.string.app_name));
 
-        recyclerView = (RecyclerView) findViewById(R.id.rv_recycler);
-
-        smartphones = new SmartphoneRepository().getSmartphones();
-
-        adapter = new SmartphoneRecyclerAdapter(smartphones);
-
-        adapter.setListener(new OnSmartphoneRecyclerItemClickListener() {
-            @Override
-            public void onItemClick(View v, int position) {
-                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
-                intent.putExtra(Constants.KEY_RES_ID, position);
-                startActivity(intent);
-            }
-        });
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
+        chooserFragment = (ChooserFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_chooser);
 
 
     }
--- app/src/main/java/com/keepsolid/gittestapp/fragment/ChooserFragment.java
+++ app/src/main/java/com/keepsolid/gittestapp/fragment/ChooserFragment.java
@@ -1,6 +1,7 @@
 package com.keepsolid.gittestapp.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,10 +13,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.keepsolid.gittestapp.R;
+import com.keepsolid.gittestapp.activity.FirstActivity;
+import com.keepsolid.gittestapp.activity.SecondActivity;
 import com.keepsolid.gittestapp.adapter.SmartphoneRecyclerAdapter;
 import com.keepsolid.gittestapp.model.Smartphone;
+import com.keepsolid.gittestapp.utils.Constants;
+import com.keepsolid.gittestapp.utils.listeners.OnSmartphoneRecyclerItemClickListener;
 import com.keepsolid.gittestapp.utils.repository.SmartphoneRepository;
 
 import java.util.List;
@@ -26,6 +32,13 @@ public class ChooserFragment extends Fragment {
     private RecyclerView recyclerView;
     private SmartphoneRecyclerAdapter adapter;
     private List<Smartphone> smartphones;
+    private Listener listener;
+
+
+    static interface Listener {
+        void itemClicked(long id);
+    };
+
 
     public ChooserFragment() {
     }
@@ -33,22 +46,29 @@ public class ChooserFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        smartphones = new SmartphoneRepository().getSmartphones();
+
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_chooser, container, false);
-
         recyclerView = (RecyclerView) view.findViewById(R.id.rv_recycler);
 
-        Context context = this.getContext();
+        smartphones = new SmartphoneRepository().getSmartphones();
 
         adapter = new SmartphoneRecyclerAdapter(smartphones);
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(context));
-        recyclerView.setAdapter(adapter);
+        adapter.setListener(new OnSmartphoneRecyclerItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                Intent intent = new Intent(getActivity(), SecondActivity.class);
+                intent.putExtra(Constants.KEY_RES_ID, position);
+                startActivity(intent);
+            }
+        });
 
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(adapter);
         return view;
     }
 
