440df46679447f92bbb00aca2dbe33c833c2bf6f
alexzhuravlov13 <alexzhuravlov13@gmail.com>
2020-07-26 17:02:12 +0300

    Fixed fields verification

--- app/src/main/java/com/keepsolid/gittestapp/activity/AddActivity.java
+++ app/src/main/java/com/keepsolid/gittestapp/activity/AddActivity.java
@@ -46,43 +46,45 @@ public class AddActivity extends BaseActivity {
         Editable modelText = model.getText();
         Editable yearText = year.getText();
 
-        if (manufacturerText != null && manufacturerText.length() == 0 || modelText != null && modelText.length() == 0 || yearText != null && yearText.length() == 0) {
-            new AlertDialog.Builder(this)
-                    .setTitle("Error")
-                    .setMessage("Fill all fields")
-                    .setNeutralButton(R.string.ok, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                        }
-                    })
-                    .create()
-                    .show();
-        } else {
-            Intent intent = new Intent();
-            intent.putExtra("manufacturer", manufacturerText.toString().trim());
-            intent.putExtra("model", modelText.toString().trim());
-            int yearInt;
+        if (manufacturerText != null &&
+                modelText != null &&
+                yearText != null) {
+            if (manufacturerText.toString().trim().length() == 0 ||
+                    modelText.toString().trim().length() == 0 ||
+                    yearText.toString().trim().length() == 0) {
+                createAlertDialog("Fill all fields");
+            } else {
+                Intent intent = new Intent();
+                intent.putExtra("manufacturer", manufacturerText.toString().trim());
+                intent.putExtra("model", modelText.toString().trim());
+                int yearInt;
 
-            try {
-                String year = yearText.toString();
-                if (year.length() != 4) {
-                    throw new Exception("Year length must be 4");
+                try {
+                    String year = yearText.toString();
+                    if (year.length() != 4) {
+                        throw new Exception("Year length must be 4");
+                    }
+                    yearInt = Integer.parseInt(year);
+                    intent.putExtra("year", yearInt);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                } catch (Exception e) {
+                    createAlertDialog("Year must be in number format \"yyyy\"");
                 }
-                yearInt = Integer.parseInt(year);
-                intent.putExtra("year", yearInt);
-                setResult(RESULT_OK, intent);
-                finish();
-            } catch (Exception e) {
-                new AlertDialog.Builder(this).
-                        setTitle("Error")
-                        .setMessage("Year must be in number format \"yyyy\"")
-                        .setNeutralButton(R.string.ok, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                            }
-                        })
-                        .create()
-                        .show();
             }
         }
+    }
+
+    private void createAlertDialog(String s) {
+        new AlertDialog.Builder(this)
+                .setTitle("Error")
+                .setMessage(s)
+                .setNeutralButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                })
+                .create()
+                .show();
     }
 
     private void initViews() {
